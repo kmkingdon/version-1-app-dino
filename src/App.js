612351dd8098d640e0428436fe3ba6804d6b03b2
kmkingdon <32685092+kmkingdon@users.noreply.github.com>
2018-01-19 10:20:23 -0700

    react-create-app on heroku

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+import { Header } from './Header.js';
+import { Section } from './Section.js';
+import { Form } from './Form.js';
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      data:[],
+    }
+    this.updateData = this.updateData.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('./listings.json')
+      .then(response => response.json())
+      .then(response => this.setState({data:response}))
+  }
+
+  updateData(data) {
+    let newData = this.state.data;
+    newData.unshift(data);
+    return this.setState({data:newData});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <Section key={this.state.data.id} listings={this.state.data}/>
+          <Form updateData={this.updateData}/>
+        </main>
+      </div>
+    );
+  }
+}
---
+++ src/Form.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit= this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let newJob = {
+      title: event.target[0].value,
+      pay: event.target[1].value,
+      description: event.target[2].value,
+      interested: []
+    };
+    this.props.updateData(newJob);
+    event.target.reset();
+  }
+
+  render() {
+    return <aside id="side-bar">
+              <h3>Add a Job</h3>
+              <form className="job-form" onSubmit={this.handleSubmit}>
+                  <label htmlFor="title">Title</label>
+                  <input type="text" name="title" />
+                  <label htmlFor="pay">Compensation</label>
+                  <input type="text" name="pay" />
+                  <label htmlFor="description">Description</label>
+                  <textarea name="description" rows="8" cols="40"></textarea>
+                  <input type="submit" name="submit" value="Submit" />
+              </form>
+            </aside>
+  }
+}
